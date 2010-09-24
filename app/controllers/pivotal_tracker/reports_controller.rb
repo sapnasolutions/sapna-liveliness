6892ed50dd4b5a0ef0fd1feef188b35d529f7cb3
Aashish <crazyscorpio12@gmail.com>
2010-09-24 12:34:59 +0530

    enhancements for reports page, when no activities.

--- app/controllers/pivotal_tracker/reports_controller.rb
+++ app/controllers/pivotal_tracker/reports_controller.rb
@@ -13,6 +13,7 @@ class PivotalTracker::ReportsController < PivotalTracker::BaseController
   def create
     report = Report.new(params, session)
     @activities = report.generate
+    @member_id  = params[:pivotal_tracker_member_id]
     render :update do |page|
       page.replace_html("pivotal-content", render(:partial => "pivotal_tracker/reports/activities"))
     end
