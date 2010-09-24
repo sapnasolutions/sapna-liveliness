af2b35ffeff4f40d7d6c04cc9fea03b05ac9228e
Aashish <crazyscorpio12@gmail.com>
2010-09-24 12:22:12 +0530

    minor fixes and UI fixes too

--- app/controllers/pivotal_tracker/sessions_controller.rb
+++ app/controllers/pivotal_tracker/sessions_controller.rb
@@ -9,30 +9,29 @@ class PivotalTracker::SessionsController < PivotalTracker::BaseController
   end
   
   def create
-    token = get_token(params[:pivotal_tracker])
+    @token = get_token(params[:pivotal_tracker])
+    @projects = load_projects(@token) if @token
     render :update do |page|    
-      if token.present?
-        session[:pivotal_tracker_token] = token
-        session[:pivotal_tracker_username] = params[:pivotal_tracker][:login] if token
-        render :partial => "pivotal_tracker/projects/projects", :locals => { :projects => load_projects(token) }, :layout => false
+      if @token.present?
+        session[:pivotal_tracker_token] = @token
+        session[:pivotal_tracker_username] = params[:pivotal_tracker][:login]
+        page.replace_html("pivotal-content", :partial => "pivotal_tracker/projects/projects")
       else
+        flash.now[:error] = "Pivotal Tracker could not authenticate you with the credentials provided. Please try again."
       end
+      page.replace "pivotal_tracker_menu", :partial => "pivotal_tracker/menu"
+      page.replace_html "messages", :partial => "/layouts/messages"
     end
   end
   
   def destroy
     session[:pivotal_tracker_token], session[:pivotal_tracker_username] = nil
     render :update do |page|
-      page.redirect_to root_path
+      page.replace "pivotal_tracker_menu", :partial => "pivotal_tracker/menu"
+      page.replace_html "messages", :partial => "/layouts/messages"
+      page.replace_html("pivotal-content", "")
     end
     
-  end
-  
-  private
-  
-  def get_token(params)
-    x=PivotalTracker::Token.get_token(params[:login], params[:password])
-    x
   end
   
 end
